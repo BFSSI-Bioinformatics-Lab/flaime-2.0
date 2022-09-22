6fff8aa169aeb5de132478f7655378f273a34925
stephanie <bfssi.stephanie.diaz@gmail.com>
2022-09-22 10:50:03 -0400

    Added pages

---
+++ src/components/pages/tools/Product_browser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product_browser = () => {
+  return (
+    <div>
+        <h2>Product_browser Page</h2>
+    </div>
+  )
+}
+
+export default Product_browser
---
+++ src/components/pages/tools/Report_builder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Report_builder = () => {
+  return (
+    <div>
+        <h2>Report_builder Page</h2>
+    </div>
+  )
+}
+
+export default Report_builder
