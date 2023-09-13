71a4efb5b7b598b05f658c32b4b3eda96a1661d6
Penguinfami <christine.spyphone.zhao@gmail.com>
2023-09-13 15:04:17 -0400

    added styled landing page sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,74 @@
 import { Container } from '@mui/system'
 import React from 'react'
+import { Link } from "react-router-dom";
 import { Image } from 'mui-image'
 import landing from "../static/images/landing.jpg"
 import quicklinks from "../static/images/quickLinks.jpg"
-import { Typography, Box, Paper } from '@mui/material'
+import { Typography, Box, Paper, IconButton } from '@mui/material'
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
+import ShoppingCardIcon from "@mui/icons-material/ShoppingCart";
+import ArrowCircleRight from "@mui/icons-material/ArrowCircleRight";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: `${theme.spacing(1)} ${theme.spacing(3)}`,
   textAlign: 'center',
-  color: theme.palette.text.secondary,
-  height:'100px',
-  width: '200px',
+  color: theme.palette.primary.dark,
+  height:'187px',
+  width: '410px',
   display: "flex",
-  flexDirection: "column",
-  justifyContent: "center"
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  borderRadius: '24px',
+  fontSize: "30px"
 }));
 
+const ArrowButtonLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.light,
+  fontSize: 0,
+}));
 
+const ArrowButton = styled(IconButton)(({ theme }) => ({
+  position: "absolute",
+  transform: "translate(350px, 60px)",
+}));
 
 const Home = () => {
+  const theme = useTheme();
+
+  const exploreItems = [
+    {
+      title: "Get reports by Stores",
+      icon: <ShoppingCardIcon sx={{ fontSize: 77 }} />,
+      link: ""
+    },
+    {
+      title: "Explore all products",
+      icon: <ShoppingCardIcon sx={{ fontSize: 77 }} />,
+      link: ""
+    },
+    {
+      title: "Get reports by Category",
+      icon: <ShoppingCardIcon sx={{ fontSize: 77 }} />,
+      link: ""
+    },
+    {
+      title: "Explore all data and data history",
+      icon: <ShoppingCardIcon sx={{ fontSize: 77 }} />,
+      link: ""
+    },
+    {
+      title: "Get reports by Nutrient",
+      icon: <ShoppingCardIcon sx={{ fontSize: 77 }} />,
+      link: ""
+    },
+  ]
+
   return (
-    <div>
+    <div style={{ backgroundColor: theme.palette.landing.main }}>
       <div className='landing-img' style={{ position: "relative" }}>
         <Image src={landing} height='550px' duration={0} easing='ease' opacity={0.5}></Image>
         
@@ -33,47 +77,49 @@ const Home = () => {
         </Typography>
       </div>
       
-      <Container>
-        <Typography variant='h4'>Explore FLAIME</Typography>
+      <Container maxWidth="sm" sx={{ padding: "30px"}}>
+        <Typography variant="h2" color="primary" align="center">Explore the FLAIME Database</Typography>
       </Container>
-      <div style={{height: "50vh", backgroundImage:`url(${quicklinks})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+      <div style={{backgroundImage:`url(${quicklinks})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
         <Box
           display="flex"
           justifyContent="center"
           alignItems="center"
-          style={{ minHeight: "50vh", backgroundColor: 'rgba(241, 241, 241, 0.4)'}}
+          style={{ minHeight: "50vh", backgroundColor: 'rgba(241, 241, 241, 0.4)', padding: "2vh 0"}}
         >
           
-            <Grid container rowSpacing={{xs: 1, sm: 2, md: 3}} 
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }} direction="row">
-              <Grid item xs={6} sm={4} md={4} align="center">
-                <Item>
-                <Typography>Item 1</Typography>
-                </Item>
-              </Grid>
-              <Grid item xs={6} sm={4} md={4} align="center">
-                <Item><Typography>Item 2</Typography></Item>
-              </Grid>
-              <Grid item xs={6} sm={4} md={4} align="center">
-                <Item><Typography>Item 3</Typography></Item>
-              </Grid>
-            {/* </Grid>
-            <Grid container spacing={2} direction="row"> */}
-              <Grid item xs align="center">
-                <Item><Typography> Item 4</Typography></Item>
-              </Grid>
-              <Grid item xs align="center">
-                <Item><Typography> Item 5</Typography></Item>
-              </Grid>
+            <Grid container 
+              rowSpacing={{xs: 1, sm: 2, md: 6}} 
+              columnSpacing={{ xs: 1, sm: 2, md: 6 }}
+              direction="row"
+              alignItems="center"
+              justifyContent="space-evenly"
+            >
+              {exploreItems.map((item, index) => 
+                  <Grid item key={item.title}
+                    xs={12} 
+                    sm={12} 
+                    md={6} 
+                    lg={4}
+                    align="center"
+                  >
+                    <Item>
+                      {item.icon}
+                      <Typography variant="subtitle" align="left" sx={{ margin: "0 42px" }}>{item.title}</Typography>
+                      <ArrowButton>
+                        <ArrowButtonLink>
+                          <ArrowCircleRight sx={{ fontSize: 44 }}/>
+                        </ArrowButtonLink>
+                      </ArrowButton>
+                    </Item>
+                  </Grid>
+                )
+              }
             </Grid> 
           
           
         </Box>  
       </div>
-      
-      <Container>
-        <h2>Footer</h2>
-      </Container>
       
     </div>
   )
