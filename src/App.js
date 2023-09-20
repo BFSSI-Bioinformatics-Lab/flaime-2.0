4d73fe63181715a369a01fcdea1374932b8a5cf7
Penguinfami <christine.spyphone.zhao@gmail.com>
2023-09-20 16:35:29 -0400

    Fix merge conflict

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 // import Table from "./components/Table";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from '@mui/material/styles';
-import Theme from "./components/Theme";
-=======
 import Header from "./components/page/Header";
 import Footer from "./components/page/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import Theme from "./components/theme/Theme";
->>>>>>> theme-api-provider
 import Batch_browser from "./pages/tools/Batch_browser";
 import Product_browser from "./pages/tools/Product_browser";
 import Product_detail from "./pages/tools/Product_detail";
@@ -32,7 +24,6 @@ function App() {
       <ThemeProvider theme={Theme}>
         <div className="App">
           <Header />
-<<<<<<< HEAD
           <Routes>
             <Route path="/" element={<Home/>} />
             {/* TODO: delete this */}
@@ -57,28 +48,6 @@ function App() {
             <Route path="tools/product-browser/:productId" element={<Product_detail />} />
           </Routes>
 
-=======
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              {/* Tools Dropdown */}
-              <Route path="tools/product-browser" element={<Product_browser />} />
-              <Route path="tools/batch-browser" element={<Batch_browser />} />
-              <Route path="tools/report-builder" element={<Report_builder />} />
-              {/* Reports Dropdown */}
-              <Route path="reports/store" element={<Store_report />} />
-              <Route path="reports/category" element={<Category_report />} />
-              <Route path="reports/nutrient" element={<Nutrient_report />} />
-              {/* Data Dropdown */}
-              <Route path="data/quality" element={<Quality />} />
-              <Route path="data/download" element={<Download />} />
-              <Route path="data/visualizations" element={<Visualizations />} />
-
-              <Route path="about" element={<About />} />
-
-              {/* Product Detail Page */}
-              <Route path="tools/product-browser/:productId" element={<Product_detail />} />
-            </Routes>
->>>>>>> theme-api-provider
           <Footer />
         </div>
       </ThemeProvider>
