872db2fcb336d720e41a8ff05c2da4dfa35f63c9
Penguinfami <christine.spyphone.zhao@gmail.com>
2023-11-10 09:11:18 -0500

    Grocery report diagram initial layout

--- src/pages/reports/Category_report.js
+++ src/pages/reports/Category_report.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import GroceryLayout from '../../components/diagrams/GroceryLayout'
 const Category_report = () => {
   return (
     <div>
         <h2>Category_report Page</h2>
+        <GroceryLayout/>
     </div>
   )
 }
