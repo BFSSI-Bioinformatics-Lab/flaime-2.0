6c9a1c6d0f735b287148371eb9756f6c8682a641
stephanie <bfssi.stephanie.diaz@gmail.com>
2024-06-07 10:58:38 -0400

    testing filtering of data with elastic search on data menu pages

--- src/pages/data/Quality.js
+++ src/pages/data/Quality.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Pagination, TextField, Paper } from '@mui/material';
+import PageContainer from '../../components/page/PageContainer';
+
 
 const Quality = () => {
 
@@ -9,86 +11,77 @@ const Quality = () => {
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(15);
   const [totalProducts, setTotalProducts] = useState(0);
-  const [searchTerm, setSearchTerm] = useState('');
+  // const [searchTerm, setSearchTerm] = useState('');
+  const [idSearchTerm, setIdSearchTerm] = useState('');
+  const [storeNameSearchTerm, setStoreNameSearchTerm] = useState('');
+  const [sourceNameSearchTerm, setSourceNameSearchTerm] = useState('');
+  const [siteNameSearchTerm, setSiteNameSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-          let queryObject;
-          if (!isNaN(searchTerm)) {
-              queryObject = {
-                  "bool": {
-                      "should": [
-                          {
-                              "match": {
-                                  "most_recent_flag": {
-                                      "query": true
-                                  }
-                              }
-                          },
-                          {
-                              "match": {
-                                  "stores.name": {
-                                      "query": searchTerm,
-                                      "operator": "or"
-                                  }
-                              }
-                          },
-                          {
-                              "match": {
-                                  "site_name": {
-                                      "query": searchTerm,
-                                      "operator": "or"
-                                  }
-                              }
-                          },
-                          {
-                              "term": {
-                                  "id": {
-                                      "value": searchTerm
-                                  }
-                              }
-                          }
-                      ]
+        let queryObject = {
+          bool: {
+            should: [
+              {
+                match: {
+                  most_recent_flag: {
+                    query: true
                   }
-              };
-          } else {
-              queryObject = {
-                  "bool": {
-                      "should": [
-                          {
-                              "match": {
-                                  "most_recent_flag": {
-                                      "query": true
-                                  }
-                              }
-                          },
-                          {
-                              "match": {
-                                  "stores.name": {
-                                      "query": searchTerm,
-                                      "operator": "or"
-                                  }
-                              }
-                          },
-                          {
-                              "match": {
-                                  "site_name": {
-                                      "query": searchTerm,
-                                      "operator": "or"
-                                  }
-                              }
-                          }
-                      ]
-                  }
-              };
+                }
+              }
+            ]
           }
-
-          const response = await axios.post('http://172.17.10.96:9200/data_v1/_search', {
-              query: queryObject,
-              from: (page - 1) * rowsPerPage,
-              size: rowsPerPage
+        };
+  
+        if (idSearchTerm) {
+          queryObject.bool.should.push({
+            term: {
+              id: {
+                value: idSearchTerm
+              }
+            }
           });
+        }
+  
+        if (storeNameSearchTerm) {
+          queryObject.bool.should.push({
+            match: {
+              "stores.name": {
+                query: storeNameSearchTerm,
+                operator: "or"
+              }
+            }
+          });
+        }
+  
+        if (sourceNameSearchTerm) {
+          queryObject.bool.should.push({
+            match: {
+              "sources.name": {
+                query: sourceNameSearchTerm,
+                operator: "or"
+              }
+            }
+          });
+        }
+  
+        if (siteNameSearchTerm) {
+          queryObject.bool.should.push({
+            match: {
+              site_name: {
+                query: siteNameSearchTerm,
+                operator: "or"
+              }
+            }
+          });
+        }
+  
+        const response = await axios.post('http://172.17.10.96:9200/data_v1/_search', {
+          query: queryObject,
+          from: (page - 1) * rowsPerPage,
+          size: rowsPerPage
+        });
         const hits = response.data.hits.hits;
         setProducts(hits.map(hit => hit._source));
         setTotalProducts(response.data.hits.total.value);
@@ -96,9 +89,9 @@ const Quality = () => {
         console.error(error);
       }
     };
-
+  
     fetchProducts();
-  }, [page, rowsPerPage, searchTerm]);
+  }, [page, rowsPerPage, idSearchTerm, storeNameSearchTerm, sourceNameSearchTerm, siteNameSearchTerm]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage + 1);
@@ -109,29 +102,79 @@ const Quality = () => {
     setPage(1);
   };
 
-  const handleSearch = event => {
-    setSearchTerm(event.target.value);
+  // const handleSearch = event => {
+  //   setSearchTerm(event.target.value);
+  // };
+  const handleIdSearch = event => {
+    setIdSearchTerm(event.target.value);
+  };
+
+  const handleStoreNameSearch = event => {
+    setStoreNameSearchTerm(event.target.value);
+  };
+
+  const handleSourceNameSearch = event => {
+    setSourceNameSearchTerm(event.target.value);
+  };
+
+  const handleSiteNameSearch = event => {
+    setSiteNameSearchTerm(event.target.value);
   };
   
 
   return (
+    <PageContainer>
     <div>
       Quality
-      <Paper component="form" className="search-form">
-        <TextField
-          label="Search"
-          variant="outlined"
-          value={searchTerm}
-          onChange={handleSearch}
-          fullWidth
-        />
-      </Paper>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px' }}>
+          <TextField
+            label="Search ID"
+            variant="outlined"
+            value={idSearchTerm}
+            onChange={handleIdSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px' }}>
+          <TextField
+            label="Search Store Name"
+            variant="outlined"
+            value={storeNameSearchTerm}
+            onChange={handleStoreNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px' }}>
+          <TextField
+            label="Search Source Name"
+            variant="outlined"
+            value={sourceNameSearchTerm}
+            onChange={handleSourceNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1 }}>
+          <TextField
+            label="Search Site Name"
+            variant="outlined"
+            value={siteNameSearchTerm}
+            onChange={handleSiteNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+      </div>
       <TableContainer>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
               <TableCell>Store Name</TableCell>
+              <TableCell>Source</TableCell>
               <TableCell>Site URL</TableCell>
             </TableRow>
           </TableHead>
@@ -140,6 +183,7 @@ const Quality = () => {
               <TableRow key={product.id}>
                 <TableCell>{product.id}</TableCell>
                 <TableCell>{product.stores.name}</TableCell>
+                <TableCell>{product.sources.name}</TableCell>
                 <TableCell>{product.site_name}</TableCell>
               </TableRow>
             ))}
@@ -155,6 +199,7 @@ const Quality = () => {
         onChangeRowsPerPage={handleChangeRowsPerPage}
       />
     </div>
+    </PageContainer>
   )
 }
 
--- src/pages/data/Visualizations.js
+++ src/pages/data/Visualizations.js
@@ -1,11 +1,170 @@
 import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Pagination, TextField, Paper } from '@mui/material';
+import PageContainer from '../../components/page/PageContainer';
+
 
 const Visualizations = () => {
+
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(15);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+          let queryObject;
+          if (!isNaN(searchTerm)) {
+              queryObject = {
+                  "bool": {
+                      "should": [
+                          {
+                              "match": {
+                                  "most_recent_flag": {
+                                      "query": true
+                                  }
+                              }
+                          },
+                          {
+                              "match": {
+                                  "stores.name": {
+                                      "query": searchTerm,
+                                      "operator": "or"
+                                  }
+                              }
+                          },
+                          {
+                              "match": {
+                                  "site_name": {
+                                      "query": searchTerm,
+                                      "operator": "or"
+                                  }
+                              }
+                          },
+                          {
+                              "term": {
+                                  "id": {
+                                      "value": searchTerm
+                                  }
+                              }
+                          }
+                      ]
+                  }
+              };
+          } else {
+              queryObject = {
+                  "bool": {
+                      "should": [
+                          {
+                              "match": {
+                                  "most_recent_flag": {
+                                      "query": true
+                                  }
+                              }
+                          },
+                          {
+                              "match": {
+                                  "stores.name": {
+                                      "query": searchTerm,
+                                      "operator": "or"
+                                  }
+                              }
+                          },
+                          {
+                              "match": {
+                                  "site_name": {
+                                      "query": searchTerm,
+                                      "operator": "or"
+                                  }
+                              }
+                          }
+                      ]
+                  }
+              };
+          }
+
+          const response = await axios.post('http://172.17.10.96:9200/data_v1/_search', {
+              query: queryObject,
+              from: (page - 1) * rowsPerPage,
+              size: rowsPerPage
+          });
+        const hits = response.data.hits.hits;
+        setProducts(hits.map(hit => hit._source));
+        setTotalProducts(response.data.hits.total.value);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, [page, rowsPerPage, searchTerm]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage + 1);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  };
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+  
+
   return (
+    <PageContainer>
     <div>
-      <h2>Visualizations page</h2>
+      Quality
+      <Paper component="form" className="search-form">
+        <TextField
+          label="Search"
+          variant="outlined"
+          value={searchTerm}
+          onChange={handleSearch}
+          fullWidth
+        />
+      </Paper>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Store Name</TableCell>
+              <TableCell>Site URL</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map(product => (
+              <TableRow key={product.id}>
+                <TableCell>{product.id}</TableCell>
+                <TableCell>{product.stores.name}</TableCell>
+                <TableCell>{product.site_name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        count={Math.ceil(totalProducts / rowsPerPage)}
+        page={page - 1}
+        onChange={handleChangePage}
+        siblingCount={1}
+        rowsPerPage={rowsPerPage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </div>
+    </PageContainer>
   )
 }
+
+
+
+
+ 
 
 export default Visualizations
