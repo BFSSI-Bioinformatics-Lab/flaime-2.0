a5d9251652ff5ebee43c97e0c46f64103095d4da
stephanie <bfssi.stephanie.diaz@gmail.com>
2024-06-14 11:20:59 -0400

    product browser page updated

--- src/pages/tools/Product_browser/index.jsx
+++ src/pages/tools/Product_browser/index.jsx
@@ -1,116 +1,285 @@
-import React, { useState } from 'react'
-import { SearchStoreProductsControlled, GetAllStoreProductsByPaginationControlled } from '../../../api/services/StoreProductService';
-import { Link } from 'react-router-dom';
-import MainTable from "../../../components/table/MainTable";
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Pagination, TextField, Paper, Typography } from '@mui/material';
 import PageContainer from '../../../components/page/PageContainer';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+
+
 const Product_browser = () => {
 
   const [products, setProducts] = useState([]);
-  const [cancelSearch, setCancelSearch] = useState(() => () => {});
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(15);
+  const [totalProducts, setTotalProducts] = useState(0);
+  // const [searchTerm, setSearchTerm] = useState('');
+  const [idSearchTerm, setIdSearchTerm] = useState('');
+  const [storeNameSearchTerm, setStoreNameSearchTerm] = useState('');
+  const [sourceNameSearchTerm, setSourceNameSearchTerm] = useState('');
+  const [siteNameSearchTerm, setSiteNameSearchTerm] = useState('');
+  const [categorySearchTerm, setCategorySearchTerm] = useState('');
 
-  const columns = [
-    // { field: 'id', headerName: 'ID' },
-    // { field: 'productName', headerName: 'Product', width: 100 },
-    // { field: 'productStore', headerName: 'Store', width: 100 },
-    // { field: 'productPrice', headerName: 'Price' },
-    // { field: 'productUpc', headerName: 'UPC', width: 100 },
-    // { field: 'productCategory', headerName: 'Category', width: 100 }
-    { 
-        field: 'id', 
-        headerName: 'Id', 
-        flex: 1,
-        minWidth: 130,
-        renderCell: (params) => 
-        <Link to={`${params.row.id}`} target="_blank">{params.row.id}</Link>,
-    },
-    { 
-        field: 'siteName', 
-        headerName: 'Store Name', 
-        minWidth: 200,
-        flex: 5 
-    },
-    { 
-        field: 'rawBrand', 
-        headerName: 'Brand', 
-        minWidth: 150,
-        flex: 2 
-    },
-    { 
-        field: 'store', 
-        headerName: "Store",
-        minWidth: 150,
-        flex: 2,
-        renderCell: (params) => params.row.storeEntity.name
-    },
-    {
-        field: "categoryPredictionEntityId",
-        headerName: "Category",
-        minWidth: 100,
-        flex: 3,
-        renderCell: (params) => params.row.productEntity.categoryEntity ? params.row.productEntity.categoryEntity.name : null
-    },
-    { 
-        field: 'storeProductCode', 
-        headerName: 'Code', 
-        minWidth: 100,
-        flex: 2 
-    },
-    { 
-        field: 'siteUrl', 
-        headerName: 'External Url', 
-        minWidth: 230,
-        flex: 2,
-        renderCell: (params) => <a href={`${params.row.siteUrl}`} target="_blank" rel="noopener noreferrer">{params.row.storeEntity.name}</a>
-    },
-    { 
-      field: 'source',
-      headerName: "Data Source",
-      minWidth: 150,
-      flex: 2,
-      renderCell: (params) => params.row.sourceEntity.name
-    }
-  ];
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        let queryObject = {
+          bool: {
+            must: [
+              {
+                match: {
+                  most_recent_flag: {
+                    query: true
+                  }
+                }
+              }
+            ]
+          }
+        };
+        
+        if (idSearchTerm) {
+          queryObject.bool.must.push({
+            term: {
+              id: idSearchTerm
+            }
+          });
+        }
+  
+        if (storeNameSearchTerm) {
+          queryObject.bool.must.push({
+            match: {
+              "stores.name": {
+                query: storeNameSearchTerm,
+                operator: "and"
+              }
+            }
+          });
+        }
+  
+        if (sourceNameSearchTerm) {
+          queryObject.bool.must.push({
+            match: {
+              "sources.name": {
+                query: sourceNameSearchTerm,
+                operator: "and"
+              }
+            }
+          });
+        }
+  
+        if (siteNameSearchTerm) {
+          queryObject.bool.must.push({
+            match: {
+              site_name: {
+                query: siteNameSearchTerm,
+                operator: "and"
+              }
+            }
+          });
+        }
 
-  const getAllProducts = async (pageSize, pageNumber) => {
-    cancelSearch();
-    try {
-      const [GetAllStoreProductsByPagination, GetAllStoreProductsByPaginationCancel] = GetAllStoreProductsByPaginationControlled();
-      setCancelSearch(() => () => GetAllStoreProductsByPaginationCancel());
-      const data = await GetAllStoreProductsByPagination({ pageSize, pageNumber });
-      setProducts(data.error ? [] : data.products)
-      return data.pagination.totalRowCount;
-    } catch (e) {
-      if (e.code !== "ERR_CANCELED") {
-        return 0;
+        if (categorySearchTerm) {
+          queryObject.bool.must.push({
+            match: {
+              "categories.name": {
+                query: categorySearchTerm,
+                operator: "and"
+              }
+            }
+          });
+        }
+        
+        const elasticUrl = `${process.env.REACT_APP_ELASTIC_URL}/_search`;
+        const response = await axios.post(`${elasticUrl}`, {
+          query: queryObject,
+          from: (page - 1) * rowsPerPage,
+          size: rowsPerPage
+        });
+        const hits = response.data.hits.hits;
+        setProducts(hits.map(hit => hit._source));
+        setTotalProducts(response.data.hits.total.value);
+      } catch (error) {
+        console.error(error);
       }
-      return null;    
-    }
-  }
+    };
+  
+    fetchProducts();
+  }, [page, rowsPerPage, idSearchTerm, storeNameSearchTerm, sourceNameSearchTerm, siteNameSearchTerm, categorySearchTerm]);
 
-  const getSearchProducts = async (searchTerm, pageSize, pageNumber) => {
-    cancelSearch();
-    try {
-      const [SearchStoreProductsCall, SearchStoreProductsCancel] = SearchStoreProductsControlled();
-      setCancelSearch(() => () => SearchStoreProductsCancel());
-      const data = await SearchStoreProductsCall({ searchTerm, pageSize, pageNumber });
-      setProducts(data.error ? [] : data.products)
-      return data.pagination.totalRowCount;
-    } catch (e) {
-      if (e.code !== "ERR_CANCELED") {
-        return 0;
-      }
-      return null;    
-    }
-  }
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage + 1);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  };
+
+  // const handleSearch = event => {
+  //   setSearchTerm(event.target.value);
+  // };
+  const handleIdSearch = event => {
+    setIdSearchTerm(event.target.value);
+  };
+
+  const handleStoreNameSearch = event => {
+    setStoreNameSearchTerm(event.target.value);
+  };
+
+  const handleSourceNameSearch = event => {
+    setSourceNameSearchTerm(event.target.value);
+  };
+
+  const handleSiteNameSearch = event => {
+    setSiteNameSearchTerm(event.target.value);
+  };
+  
+  const handleCategorySearch = event => {
+    setCategorySearchTerm(event.target.value);
+  };
+  // Reset search
+  const handleReset = () => {
+    setIdSearchTerm('');
+    setStoreNameSearchTerm('');
+    setSourceNameSearchTerm('');
+    setSiteNameSearchTerm('');
+    setCategorySearchTerm('');
+  };
+
+  // Search form submit - disables reset of table when enter is pressed
+  const handleSearchFormSubmit = (event) => {
+    event.preventDefault(); // Prevent form submission
+  };
+
+  const ResetButton = styled(Button)(({ theme }) => ({
+    color: 'white',
+    backgroundColor: theme.palette.primary.dark,
+  }));
 
   return (
+    <PageContainer>
     <div>
-        {/* <h2>Product_browser Page</h2> */}
-        <PageContainer>
-          <h1>Table of all Products</h1>
-          <MainTable columns={columns} rows={products} onPageChange={getAllProducts} onSearchChange={getSearchProducts}/>
-        </PageContainer>
+      <div>
+      <Typography variant="h4" style={{ padding: '10px' }}>Product Browser</Typography>
+        <Typography variant="body1" style={{ padding: '10px', width: '80vw', margin: '0 auto' }}>
+        Search for products by ID, store name, data source, product name or category. Use the form below to search for products. Note that you can also search by more than one search term at once.
+        </Typography>
+      </div>
+      <Divider variant="middle"/>
+      <div>
+      {/* Top row of search bars */}
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', margin: '20px 20px' }}>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px', maxWidth: '300px' }} onSubmit={handleSearchFormSubmit}>
+          <TextField
+            label="Search ID"
+            variant="outlined"
+            value={idSearchTerm}
+            onChange={handleIdSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px', maxWidth: '300px' }} onSubmit={handleSearchFormSubmit}>
+          <TextField
+            label="Search by Store Name"
+            variant="outlined"
+            value={storeNameSearchTerm}
+            onChange={handleStoreNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1, marginRight: '5px', maxWidth: '300px' }} onSubmit={handleSearchFormSubmit}>
+          <TextField
+            label="Search by Data Source"
+            variant="outlined"
+            value={sourceNameSearchTerm}
+            onChange={handleSourceNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+      </div>
+      
+
+      
+      {/* Bottom row of search bars */}
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', margin: '10px 20px' }}>
+        <Paper component="form" className="search-form" 
+        style={{ flex: 1, marginRight: '5px', maxWidth: '480px'  }} onSubmit={handleSearchFormSubmit}>
+          <TextField
+            label="Search by Product Name"
+            variant="outlined"
+            value={siteNameSearchTerm}
+            onChange={handleSiteNameSearch}
+            fullWidth
+            size="small"
+          />
+        </Paper>
+        <Paper component="form" className="search-form" style={{ flex: 1, maxWidth: '480px'  }} onSubmit={handleSearchFormSubmit}>
+              <TextField
+                label="Search by category"
+                variant="outlined"
+                value={categorySearchTerm}
+                onChange={handleCategorySearch}
+                fullWidth
+                size="small"
+              />
+        </Paper>
+        {/* Reset search button */}
+        <ResetButton style={{ maxWidth: '130px', marginLeft: '5px' }}  onClick={handleReset}>Reset Search</ResetButton>
+      </div>
+      <div>
+      {[idSearchTerm, storeNameSearchTerm, sourceNameSearchTerm, siteNameSearchTerm, categorySearchTerm].some(term => term !== '') && (
+        
+        <Divider style={{ marginTop: '20px', color: '#424242' }} > 
+        Base on your search, there is a total of {totalProducts === 10000 ? "over 10,000" : totalProducts} products
+        </Divider>
+      )}
+      </div>
+      
+      
+      </div>
+      <TableContainer style={{ width: '80vw', margin: '0 auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ fontWeight: 'bold', textAlign: 'center', letterSpacing: '1px' }}>Assigned Flaime ID</TableCell>
+              <TableCell style={{ fontWeight: 'bold', textAlign: 'center', letterSpacing: '1px' }}>Store Name</TableCell>
+              <TableCell style={{ fontWeight: 'bold', textAlign: 'center', letterSpacing: '1px' }}>Data Source</TableCell>
+              <TableCell style={{ fontWeight: 'bold', textAlign: 'center', letterSpacing: '1px' }}>Product Name</TableCell>
+              <TableCell style={{ fontWeight: 'bold', textAlign: 'center', letterSpacing: '1px' }}>Category Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, index) => (
+              <TableRow key={product.id} style={{ background: index % 2 === 0 ? '#f2f2f2' : 'white'}}>
+                <TableCell style={{ width: '80px', textAlign: 'center' }}>
+                <Link to={`/tools/product-browser/${product.id}`} target="_blank">{product.id}</Link></TableCell>
+                <TableCell style={{ textAlign: 'center' }}>{product.stores.name}</TableCell>
+                <TableCell style={{ width: '140px', textAlign: 'center' }}>{product.sources.name}</TableCell>
+                <TableCell style={{ width: '375px' }}>{product.site_name}</TableCell>
+                <TableCell style={{ textAlign: 'center' }}>
+                {product.categories && product.categories[0] && product.categories[0].name ? product.categories[0].name : ''}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        count={Math.ceil(totalProducts / rowsPerPage -1)}
+        page={page - 1}
+        onChange={handleChangePage}
+        siblingCount={1}
+        rowsPerPage={rowsPerPage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+      
     </div>
+    </PageContainer>
   )
 }
 
